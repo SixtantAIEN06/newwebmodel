fda5179650026fae730550a20e89bc9f8056ee2e
fanghao1993 <40125201@gm.nfu.edu.tw>
2020-02-12 14:45:29 +0800

    first commit

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.index),
+    path('gallery/',views.gallery),
+    path('styletransfer/',views.styletransfer),
+    path('delmypic/',views.delmypic,name='uplaodfile'),
+    path('styletransfer/{imgpath:imgpath}',views.styletransfer),
+    path('result/',views.result),
+    path('objectdetection/',views.objectdetection),
+]
---
+++ home/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+from .dlib import forImport_recognize_faces_image
+import cv2
+import subprocess
+import tensorflow as tf
+
+from .flask.peeweetest import Classified
+
+from .object_detection.evaluate import YoloTest
+YoloTest = YoloTest()
+
+# Create your views here.
+def index(request):
+
+    #return HttpResponse("<p>Hello world!</p>")
+    return render(request,'index.html')
+    
+def gallery(request):
+    return render(request,'gallery.html')
+
+def styletransfer(request):
+    if request.method =='POST' and request.FILES['photoupload']:
+        myfile=request.FILES['photoupload']
+        fs = FileSystemStorage(location='home/static/images/')
+        fs.save(myfile.name,myfile)
+        # print(f'home/static/images/{myfile.name}.jpg')
+        # forImport_recognize_faces_image.readPara("home/dlib/encoding3.pickle",f'home/static/images/{myfile.name}','cnn')
+        photopath="images/upload.jpg"
+        # subprocess.run(["python", "home/object_detection/evaluate.py"])
+        
+    return render(request,'styletransfer.html',locals())
+
+def objectdetection(request):
+    if request.method =='POST' and request.FILES['photoupload']:
+        myfile=request.FILES['photoupload']
+        fs = FileSystemStorage(location='home/static/images/')
+        fs.save(myfile.name,myfile)
+        # print(f'home/static/images/{myfile.name}.jpg')
+        # forImport_recognize_faces_image.readPara("home/dlib/encoding3.pickle",f'home/static/images/{myfile.name}','cnn')
+        photopath="images/upload.jpg"
+        YoloTest.evaluate()
+        for item in YoloTest.dlist:
+            sort = Classified(**item) 
+            sort.save()
+
+    return render(request,'objectdetection.html',locals())
+
+
+def delmypic(request):
+    if request.method =='POST' and request.FILES['photoupload']:
+        myfile=request.FILES['photoupload']
+        fs = FileSystemStorage(location='home/static/images/')
+        fs.save(myfile.name,myfile)
+        # print(f'home/static/images/{myfile.name}.jpg')
+        # forImport_recognize_faces_image.readPara("home/dlib/encoding3.pickle",f'home/static/images/
+    return render(request,'delmypic.html')
+
+def result(request):
+    # if request.method =='POST':
+    return render(request,'result.html',locals())
